111710afe36235db392395f8f61c332bdad93355
aminep moulay <aminepa8@gmail.com>
2019-10-16 14:51:00 +0100

    Creation des models

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'stock.apps.StockConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/stock/models.py
+++ mysite/stock/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Categorie(models.Model):
+    CodeCategorie = models.CharField(max_length=10,primary_key=True)
+    LibelleCategorie=models.CharField(max_length=35)
+    Illustration = models.FileField(upload_to='uploads/',blank=True)
+
+
+    def __str__(self):
+        return self.LibelleCategorie
+
+    class Meta:
+        ordering = ('LibelleCategorie',)
+
+class Articles(models.Model):
+    Reference = models.CharField(max_length=10, primary_key=True)
+    Designation = models.CharField(max_length=25)
+    PrixUnitaire = models.DecimalField(decimal_places=2,max_digits=10)
+    CodeCategorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Designation
+
+    class Meta:
+        ordering = ('Designation',)
+
+class Depot(models.Model):
+    CodeDepot = models.CharField(max_length=10 , primary_key=True)
+    AdresseDepot = models.CharField(max_length=50)
+    VilleDepot = models.CharField(max_length=25)
+    Articles = models.ManyToManyField(Articles , through="stock")
+
+    def __str__(self):
+        return self.CodeDepot
+
+    class Meta:
+        ordering = ('CodeDepot',)
+
+class Stock(models.Model):
+    CodeDepot = models.ForeignKey(Depot, on_delete=models.CASCADE)
+    Reference = models.ForeignKey(Articles, on_delete=models.CASCADE)
+    QteEnStock = models.IntegerField()
+    SeuilStock = models.IntegerField()
+
+    class Meta:
+        unique_together = ['CodeDepot', 'Reference']
+
