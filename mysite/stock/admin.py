18a65227447d7c2eba44ce86d696b7d0a7a1bb44
aminep moulay <aminepa8@gmail.com>
2019-10-16 15:39:07 +0100

    remplisaage data

--- mysite/stock/admin.py
+++ mysite/stock/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Categorie,Articles,Depot,Stock
 # Register your models here.
+admin.site.register(Categorie)
+admin.site.register(Articles)
+admin.site.register(Depot)
+admin.site.register(Stock)
