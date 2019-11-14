cf5e84ca180488cb66cbea83d33f5d90d86afb4c
aminep moulay <aminepa8@gmail.com>
2019-11-14 14:01:24 +0100

    Cleaned some junk code

--- mysite/pages/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Page
-# Register your models here.
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('title','update_date')
-    ordering = ('title',)
-    search_fields = ('title',)
-admin.site.register(Page,PageAdmin) #hmmmm r or R
--- mysite/pages/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-#from django.http import HttpResponse
-# Create your views here.
-
-def index(request):
-    #return HttpResponse("<h1>Bienvenue au site de GLASSRI</h1>")
-    return render(request,"pages/page.html")
